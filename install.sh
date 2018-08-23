9909a49eeec2397e7ddb34246d4e6db93b27f6dd
yyz999 <airsharks@gmail.com>
2018-08-22 23:28:32 -0700

    V1.0

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Deploy python3 script and a runner shell to /bin/ddns_godaddy
+
+sudo rm -rf /bin/ddns_godaddy
+sudo mkdir /bin/ddns_godaddy
+sudo cp updater.py /bin/ddns_godaddy/
+sudo cp run /bin/ddns_godaddy/
+sudo chmod +x /bin/ddns_godaddy/run
+
+
+## Monitor ddns_godaddy process
+#  check process ddns_godaddy with pidfile /var/run/ddns_godaddy.pid
+#  start = "/bin/ddns_godaddy/run start"
+#  stop = "/bin/ddns_godaddy/run stop"
--- updater.py
+++ updater.py
@@ -1,12 +1,22 @@
 # Python 3
 from godaddypy import Client, Account
 import pif
+import logging
+import os
+import time
+
+def WritePid():
+    pid = str(os.getpid())
+    pid_file = open('/bin/ddns_godaddy/ddns_godaddy.pid', 'w')
+    pid_file.write(pid)
+    pid_file.close()
+    return pid
 
 def GetIpAddress():
     return pif.get_public_ip()
 
 def LoginAccount():
-    acct = Account(api_key='dLDURGMppCcr_RKTah5s9vNhDWikvvwAd8a', api_secret='test')
+    acct = Account(api_key='?', api_secret='?')
     client = Client(acct)
     return client
 
@@ -23,4 +33,15 @@ def UpdateIpAddress(client, ip):
     return ret
 
 # main
-UpdateIpAddress(LoginAccount(), GetIpAddress())
+logging.basicConfig(filename='/tmp/ddns_godaddy.log', format='%(asctime)s: %(levelname)s: %(message)s', level=logging.INFO)
+logging.info("Started: %s" % WritePid())
+current_ip = None
+updated = False
+while True:
+    new_ip = GetIpAddress()
+    updated = UpdateIpAddress(LoginAccount(), new_ip)
+    if new_ip is not current_ip:
+        logging.info('ip address changed: %s -> %s'%(current_ip, new_ip))
+    logging.info("Update: %s"%updated)
+    time.sleep(60)
+logging.warning("Exit")
